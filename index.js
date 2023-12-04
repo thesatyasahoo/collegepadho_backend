6573a8d20311babf38d595f2e0458279b7cb96a3
satya sahoo <satyasahoo525@gmail.com>
2023-12-04 21:38:47 +0530

    added code

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.use(cookieParser());
 app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
-
+app.get("", (req, res) => {
+  return res.send("Welcome to test server.");
+});
 app.use(router);
 
 const PORT = 9000;
