df35385c18a85f83e838cfc2cd7ff23968d359bc
satya sahoo <satyasahoo525@gmail.com>
2023-12-04 22:44:06 +0530

    added code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 app.get("", (req, res) => {
-  return res.send("Welcome to test server.");
+  return res.send({ message: "error", status: 403 });
 });
 app.use(router);
 
