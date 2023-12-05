a9e5eb4edb80e6b9ebe9f3407a67b2698400a008
satya sahoo <satyasahoo525@gmail.com>
2023-12-05 13:58:09 +0530

    added

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,37 +1,32 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 // const {User} = require('../models/model');
 
-
-
-const auth = async(req,res,next)=>{
-
-
-    try {
-        const getCookie = req.cookies.authToken;
-        console.log(getCookie)
-        if(getCookie.length>=10){
-            next();
-        }else{
-            res.status(200).send({message:'unauthorized'});    
-        }
-    } catch (error) {
-        res.status(200).send({message:'unauthorized'});
-        console.log('Token Not Received')
-        // next()
+const auth = async (req, res, next) => {
+  try {
+    const getCookie = req.cookies.authToken;
+    const tokenData = req?.query?.token;
+    // console.log(tokenData, "tokenData");
+    if (tokenData.length >= 10) {
+      next();
+    } else {
+      res.status(200).send({ message: "unauthorized" });
     }
+  } catch (error) {
+    res.status(200).send({ message: "unauthorized" });
+    console.log("Token Not Received");
+    // next()
+  }
+  // try {
+  //     const tokenExtracted = await jwt.verify(req.cookies.authToken, 'iambunnyandiloveblackcolorbecauseihateblackcolorbecauseiaminwhiteandiamsinglecoreprocessortaskcapable')
+  //     const userData = await User.findOne({_id:tokenExtracted.id});
+  //     req.USER_ID = userData._id;
+  //     req.ROOT_USER = userData;
+  //     next();
+  // } catch (error) {
+  //     res.status(200).send({message:'unauthorized'});
+  //     console.log('Token Not Received')
+  //     // next()
+  // }
+};
 
-
-    // try {
-    //     const tokenExtracted = await jwt.verify(req.cookies.authToken, 'iambunnyandiloveblackcolorbecauseihateblackcolorbecauseiaminwhiteandiamsinglecoreprocessortaskcapable')
-    //     const userData = await User.findOne({_id:tokenExtracted.id});
-    //     req.USER_ID = userData._id;
-    //     req.ROOT_USER = userData;
-    //     next();
-    // } catch (error) {
-    //     res.status(200).send({message:'unauthorized'});
-    //     console.log('Token Not Received')
-    //     // next()
-    // }
-}
-
-module.exports = auth
+module.exports = auth;
